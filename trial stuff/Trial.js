6a5e535b24e83e455dd1824e8dfcc708496a42ad
Alish <aboymurodovt@gmail.com>
2017-08-04 18:31:48 +1200

    This is my commit

---
+++ trial stuff/Trial.js
@@ -0,0 +1,8 @@
+alert("Go go Power rangers!");
+
+// function() {
+//     var aa= document.getElementsByTagName("input");
+//     for (var i =0; i < aa.length; i++){
+//      aa.elements[i].checked = checked;
+//     }
+// }
